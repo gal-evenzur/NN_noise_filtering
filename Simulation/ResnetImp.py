15292c9f1ed8188fe9da50e9e33f76297c16d739
Gal2 <galevenzur2@gmail.com>
2025-06-15 11:23:30 +0300

    Used chat's resnet model, bad pretty static results

--- Simulation/ResnetImp.py
+++ Simulation/ResnetImp.py
@@ -24,36 +24,71 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # %% Structure for the NN: #
 hyperVar = {
     'lr': 1e-4,
-    'n_epochs': 5,
-    'batch_size': 32,
+    'n_epochs': 20,
+    'batch_size': 64,
     'device': device,
     'n_plotted': 10,
     'start': 10,
     'optimizer': Adam,
             }
 
-class ResnetRegressor1D(nn.Module):
-    def __init__(self, f_signal=5001, output=3,pretrained=True):
+
+class BasicBlock1D(nn.Module):
+    def __init__(self, in_planes, planes, stride=1):
         super().__init__()
+        self.conv1 = nn.Conv1d(in_planes, planes, kernel_size=3, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm1d(planes)
+        self.conv2 = nn.Conv1d(planes, planes, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm1d(planes)
 
-        self.backbone = resnet18(pretrained=pretrained)
-        orig_conv = self.backbone.conv1
-
-        self.backbone.conv1 = nn.Conv1d(
-            in_channels=1,
-            out_channels=orig_conv.out_channels,
-            kernel_size=(orig_conv.kernel_size[0],),
-            stride=(orig_conv.stride[0],),
-            padding=(orig_conv.padding[0],),
-            bias=False,
-        )
-
-        self.backbone.fc = nn.Linear(self.backbone.fc.in_features, output)
-
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv1d(in_planes, planes, kernel_size=1, stride=stride),
+                nn.BatchNorm1d(planes)
+            )
 
     def forward(self, x):
-        # Expected x of size [batch, 1, 5001]
-        return self.backbone(x)
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        return F.relu(out)
+
+class ResNet18_1D(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=3):
+        super().__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv1d(1, 64, kernel_size=7, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm1d(64)
+
+        self.layer1 = self._make_layer(block, 64,  num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool1d(1)
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        layers = [block(self.in_planes, planes, stride)]
+        self.in_planes = planes
+        for _ in range(1, num_blocks):
+            layers.append(block(planes, planes))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avgpool(out).squeeze(-1)
+        out = self.fc(out)
+        return out
+
+def resnet18_1d(num_classes=3):
+    return ResNet18_1D(BasicBlock1D, [2, 2, 2, 2], num_classes)
 
 
 
@@ -71,7 +106,7 @@ test_loader = DataLoader(testSet, batch_size=hyperVar["batch_size"], shuffle=Tru
 
 losses = []
 eps = []
-model = ResnetRegressor1D(pretrained=True)
+model = resnet18_1d()
 # Here I Only intend to plot the losses graph so only need for basic event every 1/100 epochs
 def supervised_train(model, optim=SGD, device="cpu", rate = 0.01):
     model.to(device)
@@ -173,11 +208,14 @@ for i_ in range(start,start+n):
     peak_freqs = set.get_peak_freqs(i_)
     # peak_freqs = [peak_freqs[0].item(), 2000, peak_freqs[2].item()]
 
+    if X_test.ndim == 2:
+        X_test = X_test.unsqueeze(0)
+
     Y_pred = model(X_test.to(device))
 
-    Y_pred_plot = Y_pred.cpu().detach()
-    Y_test_plot = Y_test.cpu().detach()
-    X_test_plot = X_test.cpu().detach()
+    Y_pred_plot = Y_pred.cpu().detach().squeeze()
+    Y_test_plot = Y_test.cpu().detach().squeeze()
+    X_test_plot = X_test.cpu().detach().squeeze()
 
     X_test_plot = unscale_tensor(X_test_plot, X_params)
     Y_test_plot = unscale_tensor(Y_test_plot, Y_params)
